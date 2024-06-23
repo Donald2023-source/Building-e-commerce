ed8aab926ba1a3bf7bd2650e7f285bb8c9dcd0fd
Yngauxide <danieloxide4all@gmail.com>
2024-06-23 18:05:27 +0100

    signup page

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import Home from "./Pages/Home/Home"
 import { BrowserRouter, Router, Routes, Route } from "react-router-dom"
 import Nav from "./Pages/Home/nav"
 import Footer from "./Pages/Home/Footer"
+import SignUp from "./Pages/Access/SignUp/"
 import './App.css'
+
 
 const App = () => {
   return (
@@ -10,7 +12,7 @@ const App = () => {
     
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={ <Home/>} />
+        <Route path="/" element={ <SignUp/>} />
      </Routes>
      </BrowserRouter>
     </>
