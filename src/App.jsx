986e865966d840cabdef504f46ad11e40b4fe3cd
Elrey009 <daniloejike@gmail.com>
2024-07-24 12:52:02 +0100

    modified files

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,40 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import routes from "./routes/Route";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignUp from "./Pages/Access/SignUp/";
+import Home from "./Pages/Home/Home";
 import "./App.css";
-
-/*You look at the routes object in the routes file*/
-const router = createBrowserRouter([routes]);
+import Shop from "./Pages/shop/main shop/Shop";
+import ConstructionChemicals from "./Pages/shop/categories/ConstructionChemicals";
+import ConstructionMaterials from "./Pages/shop/categories/ConstructionMaterials";
+import Electronics from "./Pages/shop/categories/Electronics";
+import FarmMaterials from "./Pages/shop/categories/FarmMaterials";
+import Furniture from "./Pages/shop/categories/Furniture";
+import Paints from "./Pages/shop/categories/Paints";
+import ProductDetails from "./Pages/shop/categories/ProductDetails";
 
 const App = () => {
   return (
     <>
-      <RouterProvider router={router} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />{" "}
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="shop" element={<Shop />}>
+            <Route
+              path="shop/construction-chemicals"
+              element={<ConstructionChemicals />}
+            />
+            <Route
+              path="shop/construction-materials"
+              element={<ConstructionMaterials />}
+            />
+            <Route path="shop/electronics" element={<Electronics />} />
+            <Route path="shop/farm-materials" element={<FarmMaterials />} />
+            <Route path="shop/furniture" element={<Furniture />} />
+            <Route path="shop/paints" element={<Paints />} />
+            <Route path="shop/:id" element={<ProductDetails />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
