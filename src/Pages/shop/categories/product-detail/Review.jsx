1a5cc266b354837a8f995e382d8063712760c7b8
Elrey009 <daniloejike@gmail.com>
2024-08-02 15:48:50 +0100

    modified files

--- src/Pages/shop/categories/product-detail/Review.jsx
+++ src/Pages/shop/categories/product-detail/Review.jsx
@@ -4,88 +4,64 @@ import rate5 from "../../../../assets/rate5.png";
 import star from "../../../../assets/four-star2.png";
 
 export default function Reviews() {
+  const data = [
+    {
+      initials: "pn",
+      name: "Steve Matts",
+      date: "May 1, 2024",
+      img: star,
+    },
+    {
+      initials: "pn",
+      name: "Amira Michelle",
+      date: "May 1, 2024",
+      img: star,
+    },
+    {
+      initials: "pn",
+      name: "Jason Mormont",
+      date: "May 1, 2024",
+      img: star,
+    },
+    {
+      initials: "pn",
+      name: "Mark Klassen",
+      date: "May 1, 2024",
+      img: star,
+    },
+    {
+      initials: "pn",
+      name: "Russ Milli",
+      date: "May 1, 2024",
+      img: star,
+    },
+  ];
   return (
     <div className="w-[100%] py-6">
-      <Card className={"px-8"}>
+      <Card className={"px-8 "}>
         <h6 className="border-b-2 border-b-gray-500 pb-4">Reviews</h6>
         <div className="flex items-start gap-28 justify-start my-10">
           <img src={rate4} alt={rate4} />
           <img src={rate5} alt={rate5} />
         </div>
-        <section className="flex items-center gap-24 my-3">
-          <div className="flex items-center gap-3 text-silver">
-            <span className="font-semibold bg-black text-white p-2 text-center rounded-full">
-              PN
-            </span>
-            <div className=" flex flex-col  justify-start items-start">
-              <span className="font-bold text-black">Profile Name</span>
-              <span>May 1, 2024</span>
+        <section className="w-full flex flex-col items-start justify-start gap-8 my-3 text-nowrap">
+          {data.map(({ name, img, initials, date }, index) => (
+            <div key={index} className="flex items-start gap-24 ">
+              <div className="flex  items-start gap-6 justify-start">
+                <span className="h-12 w-12 flex items-center justify-center bg-black text-white rounded-full">
+                  {initials}
+                </span>
+                <div className="flex flex-col items-start ">
+                  <span className="font-bold text-black">{name}</span>
+                  <span>{date}</span>
+                </div>
+              </div>
+              <div className=" flex flex-col  gap-2 items-start">
+                <img src={img} alt={img} />
+                <span>Lorem ipsum dolor sit amet consectetur.</span>
+              </div>
             </div>
-          </div>
-          <div>
-            <img src={star} alt={star} />
-            <span>Lorem ipsum dolor sit amet consectetur.</span>
-          </div>
-        </section>
-        <section className="flex items-center gap-24 my-3">
-          <div className="flex items-center gap-3 text-silver">
-            <span className="font-semibold bg-black text-white p-2 text-center rounded-full">
-              PN
-            </span>
-            <div className=" flex flex-col  justify-start items-start">
-              <span className="font-bold text-black">Profile Name</span>
-              <span>May 1, 2024</span>
-            </div>
-          </div>
-          <div>
-            <img src={star} alt={star} />
-            <span>Lorem ipsum dolor sit amet consectetur.</span>
-          </div>
-        </section>
-        <section className="flex items-center gap-24 my-3">
-          <div className="flex items-center gap-3 text-silver">
-            <span className="font-semibold bg-black text-white p-2 text-center rounded-full">
-              PN
-            </span>
-            <div className=" flex flex-col  justify-start items-start">
-              <span className="font-bold text-black">Profile Name</span>
-              <span>May 1, 2024</span>
-            </div>
-          </div>
-          <div>
-            <img src={star} alt={star} />
-            <span>Lorem ipsum dolor sit amet consectetur.</span>
-          </div>
-        </section>
-        <section className="flex items-center gap-24 my-3">
-          <div className="flex items-center gap-3 text-silver">
-            <span className="font-semibold bg-black text-white p-2 text-center rounded-full">
-              PN
-            </span>
-            <div className=" flex flex-col  justify-start items-start">
-              <span className="font-bold text-black">Profile Name</span>
-              <span>May 1, 2024</span>
-            </div>
-          </div>
-          <div>
-            <img src={star} alt={star} />
-            <span>Lorem ipsum dolor sit amet consectetur.</span>
-          </div>
-        </section>
-        <section className="flex items-center gap-24 my-3">
-          <div className="flex items-center gap-3 text-silver">
-            <span className="font-semibold bg-black text-white p-2 text-center rounded-full">
-              PN
-            </span>
-            <div className=" flex flex-col  justify-start items-start">
-              <span className="font-bold text-black">Profile Name</span>
-              <span>May 1, 2024</span>
-            </div>
-          </div>
-          <div>
-            <img src={star} alt={star} />
-            <span>Lorem ipsum dolor sit amet consectetur.</span>
-          </div>
+          ))}
         </section>
       </Card>
     </div>
